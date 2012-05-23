9cee26edd1c6c2404d8042444399b152035812d2
Austin Spires <austinspires@gmail.com>
2012-05-23 13:47:38 -0500

    Initial Commit and Config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PagerdutyInterview::Application.config.secret_token = '96f29339be83a4fcc664ea0266b9bcec5757af42042eeca02ce16eeb5edbd050a80faa92fd0da465c87e774f15f906b2af36e6ccdc9c65d656a293221458edfb'
